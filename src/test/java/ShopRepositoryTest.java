913607f0f6779d444a54cf6838f0b55ddf02cc84
Vadim Zhuravlev <vadim.zhuravlev120498@mail.ru>
2024-07-05 17:03:15 +0300

    Initial commit

---
+++ src/test/java/ShopRepositoryTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopRepositoryTest {
+    Product product1 = new Product(1, "Хлеб", 39);
+    Product product2 = new Product(45, "Шоколад", 109);
+
+    @Test
+    public void removeProduct(){
+        ShopRepository remove = new ShopRepository();
+
+        remove.add(product1);
+        remove.add(product2);
+        remove.removeById(product2.getId());
+
+        Product [] expected = {product1};
+        Product [] actual = remove.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void removeNonExistentProduct(){
+        ShopRepository remove = new ShopRepository();
+
+        remove.add(product2);
+        remove.add(product1);
+
+
+        Assertions.assertThrows(NotFoundException.class, () ->{
+            remove.removeById(7);
+        });
+    }
+}
